44ebb16d35513b93d79bd16a6c0701ba9e523c35
chenshuai12619 <1014988429@qq.com>
2018-03-15 22:27:12 +0800

    [2.0.0] 充值快豆

--- seal/src/main/kotlin/com/kuaishou/hb/ui/RechargeActivity.kt
+++ seal/src/main/kotlin/com/kuaishou/hb/ui/RechargeActivity.kt
@@ -1,12 +1,11 @@
 package com.kuaishou.hb.ui
 
 import android.os.Bundle
-import android.view.View
-import android.widget.CheckedTextView
-import com.blankj.utilcode.util.SizeUtils
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentPagerAdapter
 import com.kuaishou.hb.R
-import com.kuaishou.hb.server.utils.ColorPhrase
 import com.kuaishou.hb.ui.activity.BaseActivity
+import com.kuaishou.hb.ui.fragment.RechargeFragment
 import kotlinx.android.synthetic.main.activity_recharge.*
 
 class RechargeActivity : BaseActivity() {
@@ -19,60 +18,19 @@ class RechargeActivity : BaseActivity() {
 	}
 
 	private fun initView() {
-		recharge_ctv1.text = getFormatString(10)
-		recharge_ctv2.text = getFormatString(50)
-		recharge_ctv3.text = getFormatString(100)
-		recharge_ctv4.text = getFormatString(500)
-		recharge_ctv5.text = getFormatString(1000)
-		recharge_ctv6.text = ColorPhrase.from("自定义价格\n{售价:--元}")
-				.innerColor(R.color.black_textview)
-				.outerColor(R.color.black_textview)
-				.innerSize(SizeUtils.sp2px(12f))
-				.outSize(SizeUtils.sp2px(14f))
-				.format()
-		recharge_ctv1.tag = 10
-		recharge_ctv2.tag = 50
-		recharge_ctv3.tag = 100
-		recharge_ctv4.tag = 500
-		recharge_ctv5.tag = 1000
-		recharge_ctv6.tag = ""
+		recharge_vp.adapter = object : FragmentPagerAdapter(supportFragmentManager) {
+			override fun getItem(position: Int): Fragment {
+				return if (position == 0) {
+					RechargeFragment.newInstance("", "")
+				} else {
+					RechargeFragment.newInstance("", "")
+				}
+			}
 
-		recharge_ctv1.setOnClickListener(onClickListener)
-		recharge_ctv2.setOnClickListener(onClickListener)
-		recharge_ctv3.setOnClickListener(onClickListener)
-		recharge_ctv4.setOnClickListener(onClickListener)
-		recharge_ctv5.setOnClickListener(onClickListener)
-		recharge_ctv6.setOnClickListener(onClickListener)
-	}
-
-	private var onClickListener = View.OnClickListener {
-		if (it is CheckedTextView) {
-			recharge_money_et.setText(if (it.isChecked) {
-				it.isChecked = false
-				""
-			} else {
-				resetStatus()
-				it.isChecked = true
-				it.tag.toString()
-			})
+			override fun getCount(): Int = 2
 		}
+		recharge_tl.setupWithViewPager(recharge_vp)
 	}
 
-	private fun getFormatString(price: Int): CharSequence {
-		return ColorPhrase.from(getString(R.string.recharge_price_type, price, price))
-				.innerColor(R.color.black_textview)
-				.outerColor(R.color.black_textview)
-				.innerSize(SizeUtils.sp2px(12f))
-				.outSize(SizeUtils.sp2px(14f))
-				.format()
-	}
 
-	private fun resetStatus() {
-		recharge_ctv1.isChecked = false
-		recharge_ctv2.isChecked = false
-		recharge_ctv3.isChecked = false
-		recharge_ctv4.isChecked = false
-		recharge_ctv5.isChecked = false
-		recharge_ctv6.isChecked = false
-	}
 }
---
+++ seal/src/main/kotlin/com/kuaishou/hb/ui/fragment/RechargeFragment.kt
@@ -0,0 +1,108 @@
+package com.kuaishou.hb.ui.fragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CheckedTextView
+import com.blankj.utilcode.util.SizeUtils
+import com.kuaishou.hb.R
+import com.kuaishou.hb.server.utils.ColorPhrase
+import kotlinx.android.synthetic.main.fragment_recharge.view.*
+
+/**
+ *
+ */
+class RechargeFragment : Fragment() {
+	private lateinit var rootView: View
+	private var mParam1: String? = null
+	private var mParam2: String? = null
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		if (arguments != null) {
+			mParam1 = arguments.getString(ARG_PARAM1)
+			mParam2 = arguments.getString(ARG_PARAM2)
+		}
+	}
+
+	override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+	                          savedInstanceState: Bundle?): View? {
+		rootView = inflater!!.inflate(R.layout.fragment_recharge, container, false)
+		init()
+		return rootView
+	}
+
+	private fun init() {
+		rootView.recharge_ctv1.text = getFormatString(10)
+		rootView.recharge_ctv2.text = getFormatString(50)
+		rootView.recharge_ctv3.text = getFormatString(100)
+		rootView.recharge_ctv4.text = getFormatString(500)
+		rootView.recharge_ctv5.text = getFormatString(1000)
+		rootView.recharge_ctv6.text = ColorPhrase.from("自定义价格\n{售价:--元}")
+				.innerColor(R.color.black_textview)
+				.outerColor(R.color.black_textview)
+				.innerSize(SizeUtils.sp2px(12f))
+				.outSize(SizeUtils.sp2px(14f))
+				.format()
+		rootView.recharge_ctv1.tag = 10
+		rootView.recharge_ctv2.tag = 50
+		rootView.recharge_ctv3.tag = 100
+		rootView.recharge_ctv4.tag = 500
+		rootView.recharge_ctv5.tag = 1000
+		rootView.recharge_ctv6.tag = ""
+
+		rootView.recharge_ctv1.setOnClickListener(onClickListener)
+		rootView.recharge_ctv2.setOnClickListener(onClickListener)
+		rootView.recharge_ctv3.setOnClickListener(onClickListener)
+		rootView.recharge_ctv4.setOnClickListener(onClickListener)
+		rootView.recharge_ctv5.setOnClickListener(onClickListener)
+		rootView.recharge_ctv6.setOnClickListener(onClickListener)
+	}
+
+	private var onClickListener = View.OnClickListener {
+		if (it is CheckedTextView) {
+			rootView.recharge_money_et.setText(if (it.isChecked) {
+				it.isChecked = false
+				""
+			} else {
+				resetStatus()
+				it.isChecked = true
+				it.tag.toString()
+			})
+		}
+	}
+
+	private fun getFormatString(price: Int): CharSequence {
+		return ColorPhrase.from(getString(R.string.recharge_price_type, price, price))
+				.innerColor(R.color.black_textview)
+				.outerColor(R.color.black_textview)
+				.innerSize(SizeUtils.sp2px(12f))
+				.outSize(SizeUtils.sp2px(14f))
+				.format()
+	}
+
+	private fun resetStatus() {
+		rootView.recharge_ctv1.isChecked = false
+		rootView.recharge_ctv2.isChecked = false
+		rootView.recharge_ctv3.isChecked = false
+		rootView.recharge_ctv4.isChecked = false
+		rootView.recharge_ctv5.isChecked = false
+		rootView.recharge_ctv6.isChecked = false
+	}
+
+	companion object {
+		private val ARG_PARAM1 = "param1"
+		private val ARG_PARAM2 = "param2"
+
+		fun newInstance(param1: String, param2: String): RechargeFragment {
+			val fragment = RechargeFragment()
+			val args = Bundle()
+			args.putString(ARG_PARAM1, param1)
+			args.putString(ARG_PARAM2, param2)
+			fragment.arguments = args
+			return fragment
+		}
+	}
+}
